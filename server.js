b9460123b71d76ebfe1bf87cef38be8647494ec1
Vidit Maheshwari <viditmaheshwari14@gmail.com>
2026-03-30 01:32:18 +0530

    updated files

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const cors     = require('cors')
 require('dotenv').config()
 
 const app = express()
-app.use(cors({
-  origin: 'https://glittery-truffle-e2d1f2.netlify.app/'
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
