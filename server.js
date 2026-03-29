530feae758e9f931be7e1d5258d47980bf976dd5
Vidit Maheshwari <viditmaheshwari14@gmail.com>
2026-03-30 00:37:09 +0530

    fixed server setup

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express  = require('express')
+const express = require("express");
+const app = express();
+
 const mongoose = require('mongoose')
 const cors     = require('cors')
 require('dotenv').config()
 
-const app = express()
-
+const collegedata = require("./routes/collegeroutes");
+app.use("/college", collegedata);
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
